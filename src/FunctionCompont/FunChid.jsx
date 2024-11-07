e9f0d72708646e6e82cbcbf41194f0707e2df480
Munna Thakur <134777241+JSMUNNA1@users.noreply.github.com>
2024-11-07 14:30:25 +0530

    Update FunChid.jsx

--- src/FunctionCompont/FunChid.jsx
+++ src/FunctionCompont/FunChid.jsx
@@ -6,7 +6,8 @@ export default function FunChild({ name, age, fun }) {
       <div>{name}</div>
       <div>{age}</div>
       <button onClick={fun}>Change</button>
-      <FunGrandChild fun={fun}></FunGrandChild>
+      <FunGrandChild fun={fun}></FunGrandChild>   
+      {/* we can also change the props values and also say we can communicate with the parent using CallBack Function and passing the function in Event  */}
     </>
   );
 }
