0b02ffcc867febe336ec1f0c18597d7e08c49ff3
Munna Thakur <134777241+JSMUNNA1@users.noreply.github.com>
2024-11-07 15:02:56 +0530

    Update Parent.jsx

--- src/ClassCompont/Parent.jsx
+++ src/ClassCompont/Parent.jsx
@@ -2,13 +2,19 @@ import { Component } from "react";
 import Child from "./Child";
 
 export default class Parent extends Component {
-  state = {
-    name: "raju",
-    age: "25",
-    class: "EC1",
-  };
+ constructor(props) {
+    super(props);
+    this.state = {
+      name: "raju",
+      age: "25",
+      class: "EC1",
+    };
+  }
 
   render() {
+     this.setState({
+      age: 27,
+    });
     return (
       <>
         <Child state={this.state}></Child>
